5c473337f6f47f2280730f5e2b524bfb06ef2b4a
Rafał Dorańczyk <rafaldoranczyk@gmail.com>
2024-08-17 20:22:11 +0200

    fix: format

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,14 +1,10 @@
 import { log } from '@repo/logger';
 
-
-
 export const metadata = {
   title: 'Store | Kitchen Sink',
 };
 
-
 export default function Store() {
-
   log('Hey! This is the Store page.');
 
   return <h1>page</h1>;
